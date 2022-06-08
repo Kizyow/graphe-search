d009e9e7672aabc3cda29048bac768656a5b0e8f
Alexandre Perrot <perrot54u@etu.univ-lorraine.fr>
2022-06-08 09:13:19 +0200

    ajout getter et javadoc

--- src/Noeud.java
+++ src/Noeud.java
@@ -3,41 +3,62 @@ import java.util.List;
 
 public class Noeud {
 
+    /**
+     * Attribut représentant le nom du Noeud
+     */
     private String nom;
+    /**
+     * Attribut représentant la liste d'adjacente du Noeud
+     */
     private List<Arc> adj;
 
 
     /**
-     * constructeur noeud
+     * Constructeur du Noeud
+     *
      * @param nom nom du noeud
      */
-    public Noeud(String nom){
+    public Noeud(String nom) {
         this.nom = nom;
         adj = new ArrayList<Arc>();
     }
 
     /**
-     * redefinition de la methode equals
+     * Redéfinition de la methode equals
+     *
      * @param n objet noeud
      * @return vrai si le nom est identique a l'objet passe en param
      */
-    public boolean equals(Noeud n){
+    public boolean equals(Noeud n) {
         return getNom().equals(n.getNom());
     }
 
     /**
+     * Méthode permettant de récupérer le nom du noeud
+     *
      * @return le nom
      */
-    public String getNom(){
-        return nom;
+    public String getNom() {
+        return this.nom;
+    }
+
+    /**
+     * Méthode permettant de récupérer la liste d'adjacente du Noeud
+     *
+     * @return la liste d'adjacente sous forme d'arcs
+     */
+    public List<Arc> getAdj() {
+        return this.adj;
     }
 
     /**
      * ajoute un arc
+     *
      * @param destination nom du noeud
-     * @param cout cout pour acceder au noeud
+     * @param cout        cout pour acceder au noeud
      */
-    public void ajouterArc(String destination, double cout){
+    public void ajouterArc(String destination, double cout) {
         adj.add(new Arc(destination, cout));
     }
+
 }
