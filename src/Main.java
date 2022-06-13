ff6318a4d67789171d5ab9b2bc138e3322cf8c71
Nathan P <nat2013@laposte.net>
2022-06-13 16:10:05 +0200

    comparaison 2 algos

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
      *
      * @param args
      */
-<<<<<<< HEAD
     public static void main(String[] args) throws IOException {
 
 //        GrapheListe grapheListe = new GrapheListe();
@@ -59,29 +58,12 @@ public class Main {
          * puisqu'il ne traite pas les noeuds deja traites
          *
          * Résultats avec le Graphe1:
-         * Djikstra 36.2453
-         * BellmanFord 0.2954
+         * Djikstra 0.2954
+         * BellmanFord 36.2453
          * en millisecondes
          */
-=======
-    public static void main(String[] args) {
 
-        GrapheListe grapheListe = new GrapheListe();
-        grapheListe.ajouterArc("A", "B", 1);
-        grapheListe.ajouterArc("A", "C", 5);
-        grapheListe.ajouterArc("B", "C", 3);
-        grapheListe.ajouterArc("B", "D", 1);
-        grapheListe.ajouterArc("C", "E", 3);
-        grapheListe.ajouterArc("D", "C", 1);
-        grapheListe.ajouterArc("D", "E", 9);
-        System.out.println(grapheListe.toString());
-        System.out.println(grapheListe.toGraphviz());
 
-        BellmanFord bellmanFord = new BellmanFord();
-        Valeur valeur = bellmanFord.resoudre(grapheListe, "A");
-        System.out.println(valeur);
-        System.out.println(valeur.calculerChemin("E"));
->>>>>>> 23254b0ade4b24e86e397bdb372160ecb12b2fcd
 
     }
 
