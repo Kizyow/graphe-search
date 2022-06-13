745af19fd987d2d069d9bb387fed31a7b3941955
Nathan P <nat2013@laposte.net>
2022-06-13 14:44:15 +0200

    test

---
+++ test/TestDijkstra.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestDijkstra {
+
+    @Test
+    public void testDijkstra() {
+        // preparation des donnees
+        GrapheListe grapheListe = new GrapheListe();
+        grapheListe.ajouterArc("A", "B", 1);
+        grapheListe.ajouterArc("A", "C", 5);
+        grapheListe.ajouterArc("B", "C", 3);
+        grapheListe.ajouterArc("B", "D", 1);
+        grapheListe.ajouterArc("C", "E", 3);
+        grapheListe.ajouterArc("D", "C", 1);
+        grapheListe.ajouterArc("D", "E", 9);
+
+        // test de la methode
+        Dijkstra dijkstra = new Dijkstra();
+        Valeur valeur = dijkstra.resoudre(grapheListe, "A");
+
+        // validation
+        String resultatAttendu = "A ->  V:0.0 p:null\n" +
+                "B ->  V:1.0 p:A\n" +
+                "C ->  V:3.0 p:D\n" +
+                "D ->  V:2.0 p:B\n" +
+                "E ->  V:6.0 p:C\n";
+
+        assertEquals(resultatAttendu, valeur.toString());
+    }
+}
